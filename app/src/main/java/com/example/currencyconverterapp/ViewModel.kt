c1522bb5ad890bbe47b76022435b2d881ec945ce
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2023-07-25 13:09:14 +0300

    s

--- app/src/main/java/com/example/currencyconverterapp/ViewModel.kt
+++ app/src/main/java/com/example/currencyconverterapp/ViewModel.kt
@@ -25,6 +25,7 @@ import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
+import kotlin.math.absoluteValue
 
 
 class MainViewModel(context: Context): ViewModel() {
@@ -35,11 +36,22 @@ class MainViewModel(context: Context): ViewModel() {
     init {
         fetchDataFromNetwork()
     }
+    fun getFirstFiveElements(map: Map<String, Float>): List<Pair<String, Float>> {
+        return map.toList().take(2)
+    }
+
+    fun splitMap(map: Map<String, Float>): Pair<List<Pair<String, Float>>, List<Pair<String, Float>>> {
+        val entriesList = map.entries.toList()
+        val firstFive = entriesList.take(2).map { it.key to it.value }
+        val remaining = entriesList.drop(2).map { it.key to it.value }
+        return firstFive to remaining
+    }
+
+
     private fun getTrend(json: String?):Float?{
 
         try {
             val jsonObject = JSONObject(json)
-            println("SSSSS $jsonObject")
             val timeSeriesData = jsonObject.getJSONObject("Time Series (Daily)")
 
             // Get the latest date
@@ -65,15 +77,10 @@ class MainViewModel(context: Context): ViewModel() {
                 // Perform calculations with the rates as needed
                 val percentageChange = ((latestRate - thirtyDaysAgoRate) / thirtyDaysAgoRate) * 100f
 
-                // Now you can use the 'latestRate', 'thirtyDaysAgoRate', and 'percentageChange' as needed.
-                // For example, you can display them in a TextView or show them in a Toast message.
-                println("SSSSS Latest rate: $latestRate")
-                println("SSSSS Rate 30 days ago: $thirtyDaysAgoRate")
-                println("SSSSS Percentage change: $percentageChange")
-
                 // Create a DecimalFormat with a pattern that includes 6 digits after the decimal point
-                val decimalFormat = DecimalFormat("#.######")
+                val decimalFormat = DecimalFormat("#.##")
 
+                Log.d("SSSSSS","$percentageChange")
                 // Format the number using the DecimalFormat
                 val formattedNumber = decimalFormat.format(percentageChange).toFloat()
                 return formattedNumber
@@ -87,6 +94,28 @@ class MainViewModel(context: Context): ViewModel() {
         }
         return null
     }
+    private suspend fun fetchCurrencyData(symbol: String,baseUrl: String,endpoint: String,apiKey: String,client: OkHttpClient,exchangeRates:  Map<String, Float>): Currency? {
+        val apiUrl = "$baseUrl?$endpoint&symbol=$symbol&apikey=$apiKey"
+        val request = Request.Builder().url(apiUrl).build()
+
+        val response = withContext(Dispatchers.IO) {
+            client.newCall(request).execute()
+        }
+
+        if (response.isSuccessful) {
+            val jsonResponse = response.body()?.string()
+            val trendPercentage = getTrend(jsonResponse)
+            if (trendPercentage != null) {
+                val rate = exchangeRates[symbol] ?: 0f
+                return Currency(symbol, R.drawable.dollar, rate, trendPercentage > 0, trendPercentage, trendPercentage.absoluteValue * rate)
+            }
+        } else {
+            println("Request failed: ${response.code()} - ${response.message()}")
+        }
+
+        return null
+    }
+
     private fun fetchDataFromNetwork() {
 
         viewModelScope.launch {
@@ -100,29 +129,29 @@ class MainViewModel(context: Context): ViewModel() {
 
                 // Create an instance of OkHttpClient
                 val client = OkHttpClient()
-
-/*                val newCurrency = Currency("USD",R.drawable.dollar,10F)
-
-                // Get the current list of currencies
-                val currentCurrencies = _data.value.toMutableList()
-
-                // Add the new currency to the list
-                currentCurrencies.add(newCurrency)
-
-                // Update the _data MutableStateFlow with the updated list of currencies
-                _data.value = currentCurrencies.toList()*/
                 val response = RetrofitClient.exchangeRatesApiService.getLatestExchangeRates(RetrofitClient.ER_API_KEY)
                 var exchangeRates:  Map<String, Float> = mapOf()
                 if (response.success) {
                     exchangeRates = response.rates
 
                 }
-                for (i in 0..4){
-                    val size = exchangeRates.keys
-                    val currency = size[i]
+                val (alphaApiList, exchangeApiList) = splitMap(exchangeRates)
+                val alpha = getFirstFiveElements(exchangeRates)
+                for (currency in alpha){
+                    val symbol = currency.first
+ /*                   for (currency in alphaApiList) {
+                        val newCurrency = fetchCurrencyData(currency.first,baseUrl, endpoint,apiKey,client,exchangeRates  )
+                        if (newCurrency != null) {
+                            // Get the current list of currencies
+                            val currentCurrencies = _data.value.toMutableList()
 
-                    val symbol = currency.key
+                            // Add the new currency to the list
+                            currentCurrencies.add(newCurrency)
 
+                            // Update the _data MutableStateFlow with the updated list of currencies
+                            _data.value = currentCurrencies.toList()
+                        }
+                    }*/
                     // Create the complete API URL with query parameters
                     val apiUrl = "$baseUrl?$endpoint&symbol=$symbol&apikey=$apiKey"
 
@@ -142,12 +171,14 @@ class MainViewModel(context: Context): ViewModel() {
                             if (response.isSuccessful) {
                                 // Read the response body as a string
                                 val jsonResponse = response.body()?.string()
+                                Log.d("SSSSSS",currency.first)
                                 val trendPercentage = getTrend(jsonResponse)
                                 if(trendPercentage!=null){
-                                    val rateToUSD = exchangeRates[currency.key] ?: 0f
+                                    Log.d("SSSSSS",currency.first)
+                                    val rate = exchangeRates[symbol] ?: 0f
 
-                                    val newCurrency = Currency(currency.key,R.drawable.dollar,rateToUSD, trendPercentage>0, trendPercentage, trendPercentage*rateToUSD)
-
+                                    val newCurrency = Currency(symbol,R.drawable.dollar,rate, trendPercentage>0, trendPercentage, trendPercentage.absoluteValue*rate)
+                                    println("${newCurrency.trendProcentage}")
                                     // Get the current list of currencies
                                     val currentCurrencies = _data.value.toMutableList()
 
@@ -163,27 +194,18 @@ class MainViewModel(context: Context): ViewModel() {
                             }
                         }})
                 }
-/*                val symbols = listOf("IBM", "AAPL", "GOOGL") // Add the symbols you want to fetch data for
+                for (currency in exchangeApiList){
+                    val newCurrency = Currency(currency.first,R.drawable.dollar,currency.second, true, currency.second/1000, currency.second/10)
 
-                for (symbol in symbols) {
-                    Log.v("SSS", "Fetching data for $symbol")
-                    val response = withContext(Dispatchers.IO) {
-                        RetrofitClient.alphaApiService.getHistoricalData(symbol)
-                    }
-                    Toast.makeText(context,"$response", Toast.LENGTH_LONG).show()
+                    // Get the current list of currencies
+                    val currentCurrencies = _data.value.toMutableList()
 
-                    Log.v("SSS", "$response")
-                    *//*                    val timeSeriesData = response.timeSeries?.values?.toList()
+                    // Add the new currency to the list
+                    currentCurrencies.add(newCurrency)
 
-                                        if (timeSeriesData != null && timeSeriesData.size >= 2) {
-                                            val latestClose = timeSeriesData[0].close.toFloat()
-                                            val previousClose = timeSeriesData[1].close.toFloat()
-                                            val percentageGrowth = ((latestClose - previousClose) / previousClose) * 100f
-                                            Log.v("SSS", "Percentage growth for $symbol: $percentageGrowth")
-                                        } else {
-                                            Log.v("SSS", "Insufficient data for $symbol")
-                                        }*//*
-                }*/
+                    // Update the _data MutableStateFlow with the updated list of currencies
+                    _data.value = currentCurrencies.toList()
+                }
             } catch (e: HttpException) {
                 Toast.makeText(context,"${e.response()} ${e.message()}", Toast.LENGTH_LONG).show()
             }catch (e: Exception) {
--- app/src/main/java/com/example/currencyconverterapp/ui/MainScreen.kt
+++ app/src/main/java/com/example/currencyconverterapp/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.currencyconverterapp.ui
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -7,14 +8,22 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,32 +37,92 @@ import androidx.navigation.NavController
 import com.example.currencyconverterapp.ui.theme.*
 import kotlinx.coroutines.flow.StateFlow
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavController, currencyFlow: StateFlow<List<Currency>>){
     val currencyList by currencyFlow.collectAsState()
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        TopBar()
-        val list = listOf<SortElement>(
-            SortElement("Price", Sort.PRICE) ,
-            SortElement("Trend", Sort.TREND) ,
-            SortElement("Trend %", Sort.TREND_PERCENTAGE)
-        )
-        SortBar(Modifier.align(Alignment.CenterHorizontally),list)
+    var isDrawerOpen = remember { mutableStateOf(false) }
 
-/*        val list2 = listOf(
-            Currency("Bitcoin",R.drawable.dollar,1000.00F,*//*false,10.0F,100.00F*//*)
+    ModalNavigationDrawer(
+        drawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
+        drawerContent = {
+            // Drawer content goes here (menu items, settings, etc.)
+            Box(modifier = Modifier.background(BGWhite)){
+                Column(modifier = Modifier
+                    .padding(16.dp)
+                    .size(height = 512.dp, width = 256.dp)) {
+                    Text(
+                        text = "Filters",
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Column() {
+                        val selected by remember {mutableStateOf(false)}
+                        RadioButtonOption("Rate > 1", selected) { onSelect() }
+                        RadioButtonOption("Rate < 1", selected) { onSelect() }
+                    }
+                    // Add more menu items as needed
+                }
+            }
+        },
+        content = {
+            // Main content of your screen goes here
+            // For example, a scaffold with a top app bar
+            Scaffold(
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        TopBar(isDrawerOpen)
+                        val list = listOf<SortElement>(
+                            SortElement("Price", Sort.PRICE) ,
+                            SortElement("Trend", Sort.TREND) ,
+                            SortElement("Trend %", Sort.TREND_PERCENTAGE)
+                        )
+                        SortBar(Modifier.align(Alignment.CenterHorizontally),list)
+
+                        /*        val list2 = listOf(
+                                    Currency("Bitcoin",R.drawable.dollar,1000.00F,*//*false,10.0F,100.00F*//*)
         )*/
-        CurrencySection(currencyList, navController)
-    }
-
+                        CurrencySection(currencyList, navController)
+                    }
+                }
+            )
+        }
+    )
+}
+fun onSelect(){
 
 }
-
 @Composable
-fun TopBar(){
+fun RadioButtonOption(
+    text: String,
+    selected: Boolean,
+    onSelect: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth().padding(top = 32.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        RadioButton(
+            selected = selected,
+            onClick = null
+        )
+
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,       )
+    }
+}
+@Composable
+fun TopBar(isDrawerOpen: MutableState<Boolean>){
 
     Row(
         modifier = Modifier
@@ -64,7 +133,7 @@ fun TopBar(){
         Icon(
             painter = painterResource(id = R.drawable.baseline_menu_24),
             contentDescription = "Icon 1",
-            modifier = Modifier.clickable {  }
+            modifier = Modifier.clickable { isDrawerOpen.value = true }
         )
         Icon(
             painter = painterResource(id = R.drawable.baseline_search_24),
@@ -138,7 +207,7 @@ fun CurrencyItem(currencyItem: Currency, navigateToScreen: () -> Unit) {
             .padding(bottom = 8.dp)
             .clickable { navigateToScreen() }) {
             Image(
-                painter = painterResource(id = R.drawable.dollar),
+                painter = painterResource(id = currencyItem.image),
                 contentDescription = null,
                 modifier = Modifier
                     .size(32.dp)
@@ -160,15 +229,15 @@ fun CurrencyItem(currencyItem: Currency, navigateToScreen: () -> Unit) {
                         verticalArrangement = Arrangement.SpaceBetween
                     ) {
                         Text(text = currencyItem.name, fontSize = 16.sp, color = LightBlack)
-                        Text(text = (currencyItem.price/10).toString(), fontSize = 16.sp, color = LighterBlack)
+                        Text(text = (currencyItem.anotherPrice).toString(), fontSize = 16.sp, color = LighterBlack)
                     }
                     Column (
                         verticalArrangement = Arrangement.SpaceBetween
                     ) {
                         Text(
-                            text = (currencyItem.price/100).toString() + "%",
+                            text = (currencyItem.trendProcentage).toString() + "%",
                             fontSize = 16.sp,
-                            color = if (true /*currencyItem.trend*/) GreenTrend else RedTrend,
+                            color = if (currencyItem.trend) GreenTrend else RedTrend,
                             textAlign = TextAlign.End
                         )
                         Text(
